2418955a72ae29ede3c607248d5f40daaec18e80
UDAYVEERUS <udayveerus348567@gmail.com>
2023-11-26 09:55:02 +0530

    Add some things

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('')
+
+  const addTask = () => {
+    if(newTask.trim()!==''){
+      const newTaskObject = {
+        id : Date.now(),
+        text : newTask,
+        completed : false
+      };
+      setTasks([...tasks, newTaskObject])
+      setNewTask('')
+    }
+  }
+  const toggleTask = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todo App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type='text' placeholder='Add a new task' value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+        <button onClick={addTask}>Add</button>
+
+        </div>
+        <ul>
+          {tasks.map((task) =>(
+            <li key={task.id}>
+              <input type='checkbox' checked={task.completed} onChange={() => toggleTask(task.id)} />
+              <span style={{textDecoration:task.completed ?'line-through' : 'none'}}>{task.text}</span>
+              <button onClick={() => deleteTask(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
